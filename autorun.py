8efdd3adfc7962d80445f4d2c1a28cae434c0a96
jraynolds <jaspirian@gmail.com>
2021-12-11 05:08:43 -0800

    Separate solver and autorun files

---
+++ autorun.py
@@ -0,0 +1,21 @@
+# Plays indefinitely.
+
+import pyautogui
+from board_solver.py import solve_board
+
+PLAY_FILE_LOC = "play.png" # The local file location of the play button image
+GAME_DELAY = 2 # How much we wait before starting a new game.
+CONFIDENCE_VALUE = .9 # The confidence value we're searching the screen with. A lower number will lead to more false positives.
+
+while(True):
+	time.sleep(GAME_DELAY)
+	solve_board()
+	time.sleep(GAME_DELAY)
+	pyautogui.click(
+		pyautogui.center(
+			pyautogui.locateOnScreen(
+				PLAY_FILE_LOC, 
+				confidence=CONFIDENCE_VALUE
+			)
+		)
+	)
