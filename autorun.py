dd9a3ad3580ba60c2d3586da3094df18502d3510
jraynolds <jaspirian@gmail.com>
2022-04-04 16:36:21 -0700

    Adding a loop limit to the autorun.

--- autorun.py
+++ autorun.py
@@ -7,9 +7,12 @@ from board_solver import *
 PLAY_FILE_LOC = "play.png" # The local file location of the play button image
 GAME_DELAY = 2 # How much we wait before starting a new game.
 CONFIDENCE_VALUE = .9 # The confidence value we're searching the screen with. A lower number will lead to more false positives.
+NUM_LOOPS = 20 # The maximum number of games we'll play. After this number, the script will quit.
 
-while(True):
-	print("beginning a run...")
+i = 0
+print("beginning to play the game. We'll play " + i + " games.")
+while(i < NUM_LOOPS):
+	print("beginning game " + i + 1 + "...")
 	time.sleep(GAME_DELAY)
 	solve_board()
 	time.sleep(GAME_DELAY)
@@ -21,4 +24,6 @@ while(True):
 			)
 		)
 	)
-	print("ending a run...")
+	print("ending game " + i + 1 + "...")
+	i += 1
+print("Script exiting.")
