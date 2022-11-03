428af3e6009845e291a980217bee48f3711ba395
HjOtal-1 <102916830+HjOtal-1@users.noreply.github.com>
2022-11-03 04:37:33 +0000

    Fixed str() and 1 > 11 bugs
    Fixed str() error, Ending game 1 > Beginning game 11 bug, and rewrote a few things.

--- autorun.py
+++ autorun.py
@@ -5,17 +5,17 @@ import time
 from board_solver import *
 
 PLAY_FILE_LOC = "play.png" # The local file location of the play button image
-GAME_DELAY = 2 # How much we wait before starting a new game.
+GAME_DELAY_1 = 2.9 # How much we wait after the game loads.
+GAME_DELAY_2 = .54 # How much we wait before starting a new game.
 CONFIDENCE_VALUE = .9 # The confidence value we're searching the screen with. A lower number will lead to more false positives.
-NUM_LOOPS = 20 # The maximum number of games we'll play. After this number, the script will quit.
+NUM_LOOPS = 19 # The maximum number of games we'll play. After this number, the script will quit.
+n = 1
 
-i = 0
-print("beginning to play the game. We'll play " + i + " games.")
-while(i < NUM_LOOPS):
-	print("beginning game " + i + 1 + "...")
-	time.sleep(GAME_DELAY)
+print("Starting the game, we'll play " + str(NUM_LOOPS) + " rounds.")
+while n < NUM_LOOPS:
+	print("Beginning round #" + str(n) + "...")
 	solve_board()
-	time.sleep(GAME_DELAY)
+	time.sleep(GAME_DELAY_1)
 	pyautogui.click(
 		pyautogui.center(
 			pyautogui.locateOnScreen(
@@ -24,6 +24,7 @@ while(i < NUM_LOOPS):
 			)
 		)
 	)
-	print("ending game " + i + 1 + "...")
-	i += 1
-print("Script exiting.")
+	time.sleep(GAME_DELAY_2)
+	print("Ending round #" + str(n) + "...")
+	n += 1
+print("Exiting Script.")
